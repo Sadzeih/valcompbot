0b080849b67011caf217fa69977c7524a3e6894b
Alexis Guerville <sadzeih@gmail.com>
2022-06-30 17:21:41 +0200

    fix sentinels regex

--- sentinels.go
+++ sentinels.go
@@ -14,7 +14,7 @@ var (
 	lastSENsince    time.Time
 	lastSENsinceFmt = "The last Sentinels thread was %d days ago. 0 days since the last Sentinels thread."
 
-	sentinelsRegex = regexp.MustCompile(`(?i)\b(SEN(tinels)?)|(tenz|kanpeki|sick|dapr|(r(oc?k|awk) ?(us|as+))|shah?z(am)?|zombs)\b`)
+	sentinelsRegex = regexp.MustCompile(`(?i)\b((SEN(tinels)?)|(tenz|kanpeki|sick|dapr|(r(oc?k|awk) ?(us|as+))|shah?z(am)?|zombs))\b`)
 )
 
 func DaysSinceLastSentinelsPost(c *reddit.Client) error {
