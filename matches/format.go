0b4610bac0d1aa433bfa0e054ccb6c39db99548a
Alexis Guerville <sadzeih@gmail.com>
2022-07-08 10:27:28 +0200

    fix issues with pmt formatting

--- matches/format.go
+++ matches/format.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	MatchMd = `# [{{(index .Teams 0).Name}}](https://vlr.gg/team/{{(index .Teams 0).ID}}) {{(index .Teams 0).MapsWon}}-{{(index .Teams 1).MapsWon}} [{{(index .Teams 1).Name}}](https://vlr.gg/team/{{(index .Teams 1).ID}})
+	MatchMd = `# [{{(index .Teams 0).Name | Trim }}](https://vlr.gg/team/{{(index .Teams 0).ID}}) {{(index .Teams 0).MapsWon}}-{{(index .Teams 1).MapsWon}} [{{(index .Teams 1).Name | Trim }}](https://vlr.gg/team/{{(index .Teams 1).ID}})
 
 [vlr.gg]({{.Info.Link}})
 
@@ -20,9 +20,9 @@ var (
 
 ---
 
-**{{(index .Teams 0).Name}}** | [VLR](https://vlr.gg/team/{{(index .Teams 0).ID}})
+**{{(index .Teams 0).Name | Trim }}** | [VLR](https://vlr.gg/team/{{(index .Teams 0).ID}})
 
-**{{(index .Teams 1).Name}}** | [VLR](https://vlr.gg/team/{{(index .Teams 1).ID}})
+**{{(index .Teams 1).Name | Trim }}** | [VLR](https://vlr.gg/team/{{(index .Teams 1).ID}})
 
 ---
 
@@ -35,20 +35,27 @@ var (
 {{- range $index, $map := .Maps }}
 {{- if ne (index .Teams 0).IsWinner (index .Teams 1).IsWinner }}
 # Map {{ $index | AddOne }}: {{ $map.Name | Title }}
+{{- if eq (Add (index .Teams 0).RoundsWonAttack (index .Teams 1).RoundsWonDefense) 12 }}
 | **Team** | **ATK** | **DEF** | **Total** |
 |:----|:-----|:----|---|
-| **{{(index .Teams 0).Name}}** | {{ (index .Teams 0).RoundsWonAttack }} |  {{ (index .Teams 0).RoundsWonDefense }} | {{ (index .Teams 0).RoundsWon }}
+| **{{(index .Teams 0).Name | Trim }}** | {{ (index .Teams 0).RoundsWonAttack }} |  {{ (index .Teams 0).RoundsWonDefense }} | {{ (index .Teams 0).RoundsWon }}
 |              |          **DEF**            |            **ATK**          |
-| **{{(index .Teams 1).Name}}** | {{ (index .Teams 1).RoundsWonAttack }} |  {{ (index .Teams 1).RoundsWonDefense }} | {{ (index .Teams 1).RoundsWon }}
+| **{{(index .Teams 1).Name | Trim }}** | {{ (index .Teams 1).RoundsWonDefense }} |  {{ (index .Teams 1).RoundsWonAttack }} | {{ (index .Teams 1).RoundsWon }}
+{{- else }}
+| **Team** | **DEF** | **ATK** | **Total** |
+|:----|:-----|:----|---|
+| **{{(index .Teams 0).Name | Trim }}** | {{ (index .Teams 0).RoundsWonDefense }} |  {{ (index .Teams 0).RoundsWonAttack }} | {{ (index .Teams 0).RoundsWon }}
+|              |          **ATK**            |            **DEF**          |
+| **{{(index .Teams 1).Name | Trim }}** | {{ (index .Teams 1).RoundsWonAttack }} |  {{ (index .Teams 1).RoundsWonDefense }} | {{ (index .Teams 1).RoundsWon }}
+{{- end }}
 ---
 
-| **{{(index .Teams 0).Name}}** | **ACS** | **K** | **D** | **A** |
+| **{{(index .Teams 0).Name | Trim }}** | **ACS** | **K** | **D** | **A** |
 |------------------|---------|-------|-------|-------|
 {{- range $playerIdx, $player := (index .Teams 0).Players }}
 | [{{ $player.Alias }}](https://vlr.gg/player/{{ $player.PlayerID }}) **{{ $player.Stats.Agent | Title }}** | {{ $player.Stats.ACS }} | {{ $player.Stats.Kills }} | {{ $player.Stats.Deaths }} | {{ $player.Stats.Assists }}|
 {{- end }}
-
-| **{{(index .Teams 1).Name}}** | **ACS** | **K** | **D** | **A** |
+| **{{(index .Teams 1).Name | Trim }}** | **ACS** | **K** | **D** | **A** |
 |------------------|---------|-------|-------|-------|
 {{- range $playerIdx, $player := (index .Teams 1).Players }}
 | [{{ $player.Alias }}](https://vlr.gg/player/{{ $player.PlayerID }}) **{{ $player.Stats.Agent | Title }}** | {{ $player.Stats.ACS }} | {{ $player.Stats.Kills }} | {{ $player.Stats.Deaths }} | {{ $player.Stats.Assists }}|
@@ -65,6 +72,12 @@ var (
 func (m *Match) ToMarkdown() (string, error) {
 	funcMap := template.FuncMap{
 		"Title": strings.Title,
+		"Trim": func(s string) string {
+			return strings.Trim(s, "\t ")
+		},
+		"Add": func(a int, b int) int {
+			return a + b
+		},
 		"AddOne": func(i int) int {
 			return i + 1
 		},
