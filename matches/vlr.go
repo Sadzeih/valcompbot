f59a9e44ede0ab0aa35929310a58a39bef6ffccd
Alexis Guerville <sadzeih@gmail.com>
2022-07-14 22:09:48 +0200

    fix panic when fewer than 15 upcoming matches

--- matches/vlr.go
+++ matches/vlr.go
@@ -49,7 +49,7 @@ func GetUpcoming() ([]Upcoming, error) {
 		upcomingMatches.Matches[idx].Match = matchRes
 	}
 
-	return upcomingMatches.Matches[:15], nil
+	return upcomingMatches.Matches[:math.Min(len(upcomingMatches.Matches), 15)], nil
 }
 
 func GetMatch(id string) (*Match, error) {
